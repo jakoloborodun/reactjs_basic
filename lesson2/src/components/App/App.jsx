cd1c8f83f4020fb16c8f00d1d03cb92c647c111c
Johann <jakoloborodun@gmail.com>
2021-02-24 22:20:37 +0100

    lesson2: Divide Messages to 2 components, use Message as object.

--- lesson2/src/components/App/App.jsx
+++ lesson2/src/components/App/App.jsx
@@ -1,16 +1,16 @@
-import { Component } from 'react';
-import { Messages } from '../Messages';
+import {Component} from 'react';
+import {MessageField} from '../Messages';
 
 class App extends Component {
-    render() {
-        return (
-            <div id='test-id'>
-                <h2>Hello from React</h2>
+  render() {
+    return (
+        <div id='test-id'>
+          <h2>Hello from React</h2>
 
-                <Messages />
-            </div>
-        );
-    }
+          <MessageField/>
+        </div>
+    );
+  }
 }
 
-export { App };
+export {App};
