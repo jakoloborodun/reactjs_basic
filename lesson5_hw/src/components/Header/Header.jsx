3ff343221d049eb1f1741aa3eb0d4e3f7f6525af
Johann <jakoloborodun@gmail.com>
2021-03-07 16:28:49 +0100

    lesson5: Copy from the l4.

---
+++ lesson5_hw/src/components/Header/Header.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  ButtonGroup,
+  Button
+} from '@material-ui/core'
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+  title: {
+    flexGrow: 1,
+  },
+};
+
+class _Header extends Component {
+  render() {
+    const { classes } = this.props;
+    console.log(classes);
+
+    return (
+        <AppBar position="static">
+          <Toolbar variant="dense">
+            <Typography variant="h6" className={ classes.title }>
+              { this.props.title }
+            </Typography>
+            <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group">
+              <Button href={ "/profile" }>Profile</Button>
+              <Button href={ "/" }>Chats</Button>
+            </ButtonGroup>
+
+          </Toolbar>
+        </AppBar>
+    )
+  }
+}
+
+const Header = withStyles(styles)(_Header);
+
+export { Header }
